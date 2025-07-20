31067d751edf09004d0126becd3068113a4c2a4d
Faisal Ahmed <faisalg2006@gmail.com>
2025-07-21 01:32:42 +0200

    This commit fixes issue in github actions.

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 import matplotlib.pyplot as plt
+import chromedriver_autoinstaller
 
 def setup_driver():
+    chromedriver_autoinstaller.install()
+
     options = Options()
-    options.add_argument("--headless")  # Run in background
-    options.add_argument("--disable-cache")
-    options.add_argument("--disable-application-cache")
-    options.add_argument("--incognito")  # Helps simulate cache-less state
+    options.add_argument("--headless=new")  # new headless mode is more stable
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=options)
     return driver
